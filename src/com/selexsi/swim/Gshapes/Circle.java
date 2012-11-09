e5d4c469dfaf5c5dc5304d462874ccc45cc54ef0
luigimartin.way <luigimartin.way@gmail.com>
2012-11-09 09:37:05 +0000

    Share project "G_shapes" into "https://gshapes.googlecode.com/svn"

---
+++ src/com/selexsi/swim/Gshapes/Circle.java
@@ -0,0 +1,42 @@
+package com.selexsi.swim.Gshapes;
+
+
+public class Circle extends Shape {
+
+
+	public Circle(){
+		super();
+		calculateCoordinates();
+	}
+	
+	public Circle(double _center_lat, double _center_lon, double _radius,
+			double _height) {
+		super(_center_lat, _center_lon, _radius, _height);
+		calculateCoordinates();
+	}
+
+	public Circle(double _center_lat, double _center_lon, double _radius) {
+		super(_center_lat, _center_lon, _radius);
+		calculateCoordinates();
+	}
+
+
+	
+	@Override
+	protected void calculateCoordinates(){
+		this.coordinates = new Coordinates();
+		double lat,lon,dlon_rad,radius_deg;
+		radius_deg=radius/60; // convert radius from nautic miles to degrees
+		for(int i=0; i<=360; i++){
+			lat = Math.asin(Math.sin((center_lat*Math.PI)/180)*Math.cos(radius_deg*Math.PI/180) + Math.cos((center_lat*Math.PI)/180)*Math.sin(radius_deg*Math.PI/180)*Math.cos((i*Math.PI)/180));
+			dlon_rad = Math.atan2(Math.sin((i*Math.PI)/180)*Math.sin(radius_deg*Math.PI/180)*Math.cos((center_lat*Math.PI)/180), Math.cos(radius_deg*Math.PI/180)-Math.sin((center_lat*Math.PI)/180)*Math.sin(lat));
+			lon=(center_lon*Math.PI)/180 + dlon_rad;
+			lat=lat*180/Math.PI;
+			lon=lon*180/Math.PI;
+		this.coordinates.add(lat, lon, this.height);	
+		}
+	}
+	
+	
+
+}
---
+++ src/com/selexsi/swim/Gshapes/Coordinates.java
@@ -0,0 +1,88 @@
+package com.selexsi.swim.Gshapes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Coordinates {
+
+	private List<coordinate> coordinates;
+	
+	public Coordinates(){
+		coordinates  = new ArrayList<coordinate>();		
+	}
+	
+	public void add(Double _lat , Double _lon , Double _height){ 
+		coordinate c= new coordinate(_lat, _lon, _height);
+		coordinates.add(c);
+	}
+	
+	public void add(double _lat , double _lon ){ 
+		coordinate c= new coordinate(_lat, _lon, 0D);
+		coordinates.add(c);
+	}
+	
+	public void add(String s_lat , String s_lon , String s_height){ 
+		double _lat = Double.parseDouble(s_lat);
+		double _lon = Double.parseDouble(s_lon);
+		double _height = Double.parseDouble(s_height);
+		coordinate c= new coordinate(_lat, _lon, _height);
+		coordinates.add(c);
+	}
+	
+	public void add(String s_lat , String s_lon){ 
+		double _lat = Double.parseDouble(s_lat);
+		double _lon = Double.parseDouble(s_lon);
+		coordinate c= new coordinate(_lat, _lon, 0D);
+		coordinates.add(c);
+	}
+	
+	public coordinate get(int i){
+		return coordinates.get(i);
+	}
+	
+	public String Coordinates2String(){
+		String _toString="";
+		for(int i=0; i<coordinates.size(); i++)
+			_toString= _toString + coordinates.get(i).coordinates2String();
+ 
+		return _toString;
+	}
+	
+	public class coordinate{
+	private double Lat;
+	private double Lon;
+	private double Height;
+	
+	public coordinate(double _lat , double _lon , double _height){
+		this.Lat=_lat;
+		this.Lon=_lon;
+		this.Height= _height;
+	}
+	
+	public double getLat() {
+		return Lat;
+	}
+	public void setLat(double lat) {
+		Lat = lat;
+	}
+	public double getLon() {
+		return Lon;
+	}
+	public void setLon(double lon) {
+		Lon = lon;
+	}
+	public double getHeight() {
+		return Height;
+	}
+	public void setHeight(double height) {
+		Height = height;
+	}
+	public String coordinates2String(){
+		return Lon + "," + Lat + "," + Height + " \n ";
+	}
+	
+}
+	
+	
+	
+}
